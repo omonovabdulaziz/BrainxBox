fa984352a68f82afb24305738e00e0a28697c2d8
omonovabdulaziz <omonov2006omonov@gmail.com>
2024-01-01 16:15:57 +0500

    fixprob

--- BrainBox/src/main/java/it/live/brainbox/repository/SubtitleRepository.java
+++ BrainBox/src/main/java/it/live/brainbox/repository/SubtitleRepository.java
@@ -3,6 +3,7 @@ package it.live.brainbox.repository;
 import it.live.brainbox.entity.SubtitleWord;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,4 +14,5 @@ import java.util.UUID;
 public interface SubtitleRepository extends JpaRepository<SubtitleWord, UUID> {
     void deleteAllByMovieId(Long movie_id);
 
+    Page<SubtitleWord> findAllByMovieId(Long movie_id, Pageable pageable);
 }
