5a3916120a27f02529e8e98905b65658da7cd28e
omonovabdulaziz <omonov2006omonov@gmail.com>
2024-01-04 23:56:14 +0500

    tetete

--- telegramobot/bot/message.js
+++ telegramobot/bot/message.js
@@ -1,20 +1,14 @@
-const { bot } = require('./bot');
-const { start } = require("./helper/start");
-const { get_all_movies } = require("./helper/kinolar");
-const { get_all_movie_for_subtitle } = require('./helper/subtitle');
-const { get_all_serials } = require("./helper/serial");
-const { editOnOf } = require("./helper/developer");
+const {bot} = require('./bot');
+const {start} = require("./helper/start");
+const {get_all_movies} = require("./helper/kinolar");
+const {get_all_movie_for_subtitle} = require('./helper/subtitle');
+const {get_all_serials} = require("./helper/serial");
+const {editOnOf} = require("./helper/developer");
 
 bot.on('message', async msg => {
     const chatId = msg.chat.id;
     const text = msg.text;
 
-    const messages = await bot.getChatMessages(chatId);
-    if (messages.length > 2) {
-        for (let i = 0; i < messages.length - 2; i++) {
-            await bot.deleteMessage(chatId, messages[i].message_id);
-        }
-    }
 
     if (text === '/start') {
         await start(chatId);
@@ -30,5 +24,11 @@ bot.on('message', async msg => {
     }
     if (text === 'DeveloperModeEditor') {
         await editOnOf(chatId);
+    }
+    const messages = await bot.getChatMessages(chatId);
+    if (messages.length > 2) {
+        for (let i = 0; i < messages.length - 2; i++) {
+            await bot.deleteMessage(chatId, messages[i].message_id);
+        }
     }
 });
